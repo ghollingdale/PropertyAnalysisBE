912ad9bac674f8d61027d0da254a88cb387f9d5d
George Hollingdale <g.hollingdale@outlook.com>
2023-07-27 16:00:19 +0200

    Now contains a working API for model.

---
+++ app.py
@@ -0,0 +1,79 @@
+from pydantic import BaseModel
+from fastapi import FastAPI
+import uvicorn
+import pickle
+import json
+
+class propmodel(BaseModel):
+    number_of_bedrooms: float
+    living_area: float
+    fully_equipped_kitchen: float
+    furnished: float
+    terrace: float
+    garden: float
+    total_property_area: float
+    total_land_area: float
+    swimming_pool: float
+    state_of_the_building: float
+
+
+app = FastAPI()
+pickle_in = open('data/model.pkl', 'rb')
+regressor = pickle.load(pickle_in)
+
+# http://127.0.0.1:8000
+@app.get('/')
+def home():
+    return {"Intro" : "Welcome to the model. To use this model you need to input the following parameters as a json"}
+
+# http://127.0.0.1:8000/pricepredict
+@app.post('/pricepredict')
+def predict(data: propmodel):
+    data = data.model_dump_json()
+    data = json.loads(data)
+    print(data)    
+    
+    number_of_bedrooms = data['number_of_bedrooms']
+    living_area = data['living_area']
+    fully_equipped_kitchen = data['fully_equipped_kitchen']
+    furnished = data['furnished']
+    terrace = data['terrace']
+    garden = data['garden']
+    total_property_area = data['total_property_area']
+    total_land_area = data['total_land_area']
+    swimming_pool = data['swimming_pool']
+    state_of_the_building = data['state_of_the_building']
+    
+    prediction = regressor.predict([[
+        number_of_bedrooms,
+        living_area,
+        fully_equipped_kitchen,
+        furnished,
+        terrace,
+        garden,
+        total_property_area,
+        total_land_area,
+        swimming_pool,
+        state_of_the_building
+    ]])
+    return {
+        'prediction': round(prediction[0],2)
+        }
+## problems with /docs executions
+
+"""    print(
+        "You can expect the price to be", 
+        regressor.predict([[
+            number_of_bedrooms,
+            living_area,
+            fully_equipped_kitchen,
+            furnished,
+            terrace,
+            garden,
+            total_property_area,
+            total_land_area,
+            swimming_pool,
+            state_of_the_building
+        ]]),   
+        "€ for a property with the above variables."
+    )"""
